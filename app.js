ada1c3a4e39c8e1d2e0549bc7bb99b1a99aa1e12
ALINAFIRDOUS <alinafirdous2008@gmail.com>
2026-05-25 15:32:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,161 @@
+const billAmount = document.getElementById("bill");
+const customTipPercentage = document.getElementById("custom");
+const numberofpeople = document.getElementById("people");
+const billTipAmount = document.getElementById("tipAmount");
+const billTotalPerPerson = document.getElementById("total");
+const totalTipDisplay = document.getElementById("totalTip");
+const grandTotalDisplay = document.getElementById("grandTotal");
+const resetButton = document.getElementById("resetBtn");
+const buttons = document.querySelectorAll(".tip-btns button");
+
+function validateInputs(bill, tip, people) {
+  let isValid = true;
+
+  // Bill validation
+  if (isNaN(bill) || bill <= 0) {
+    document.getElementById("billError").textContent =
+      "Bill must be greater than 0";
+    isValid = false;
+  } else {
+    document.getElementById("billError").textContent = "";
+  }
+
+  // Tip validation (only if user entered something)
+  if (!isNaN(tip)) {
+    if (tip < 0 || tip > 100) {
+      document.getElementById("tipError").textContent = "Tip must be 0–100";
+      isValid = false;
+    } else {
+      document.getElementById("tipError").textContent = "";
+    }
+  } else {
+    document.getElementById("tipError").textContent = "";
+  }
+
+  // People validation
+  if (numberofpeople.value === "" || isNaN(people)) {
+    document.getElementById("peopleError").textContent = "";
+  } else if (people < 1 || !Number.isInteger(people)) {
+    document.getElementById("peopleError").textContent = "Min 1 person";
+    isValid = false;
+  } else {
+    document.getElementById("peopleError").textContent = "";
+  }
+  return isValid;
+}
+
+//automatic tip calculation when clicked on tip % button
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    buttons.forEach((btn) => btn.classList.remove("selected"));
+    customTipPercentage.classList.remove("custom-active");
+    e.target.classList.add("selected");
+    customTipPercentage.value = "";
+
+    let tipvalue = e.target.innerText.replace("%", "");
+
+    if (billAmount.value === "") return;
+    const people =
+      numberofpeople.value === "" ? 1 : parseInt(numberofpeople.value);
+
+    calculateTip(parseFloat(billAmount.value), parseInt(tipvalue), people);
+  });
+});
+
+billAmount.addEventListener("input", () => {
+  const bill = parseFloat(billAmount.value);
+  const people =
+    numberofpeople.value === "" ? 1 : parseInt(numberofpeople.value);
+
+  let tip = parseFloat(customTipPercentage.value);
+
+  const selectedBtn = document.querySelector(".tip-btns .selected");
+  if (!tip && selectedBtn) {
+    tip = parseInt(selectedBtn.innerText.replace("%", ""));
+  }
+  calculateTip(bill, tip, people);
+});
+
+numberofpeople.addEventListener("input", () => {
+  const bill = parseFloat(billAmount.value);
+  const people = parseInt(numberofpeople.value);
+
+  let tip = parseFloat(customTipPercentage.value);
+
+  const selectedBtn = document.querySelector(".tip-btns .selected");
+  if (!tip && selectedBtn) {
+    tip = parseInt(selectedBtn.innerText.replace("%", ""));
+  }
+
+  calculateTip(bill, tip, people);
+});
+
+//automatic tip calculation when user gives custom tip %
+customTipPercentage.addEventListener("input", (e) => {
+  buttons.forEach((btn) => btn.classList.remove("selected"));
+  customTipPercentage.classList.add("custom-active");
+
+  if (billAmount.value === "") {
+    resetEverything();
+    return;
+  }
+  const people =
+    numberofpeople.value === "" ? 1 : parseInt(numberofpeople.value);
+
+  calculateTip(
+    parseFloat(billAmount.value),
+    parseFloat(e.target.value),
+    people,
+  );
+});
+
+//to caluclate tip
+function calculateTip(billAmount, tipPercentage, numberofpeople) {
+  const isValid = validateInputs(billAmount, tipPercentage, numberofpeople);
+
+  if (!isValid) {
+    billTipAmount.innerHTML = "₹0.00";
+    billTotalPerPerson.innerHTML = "₹0.00";
+    totalTipDisplay.innerHTML = "₹0.00";
+    grandTotalDisplay.innerHTML = "₹0.00";
+    return;
+  }
+
+  //total tip
+  const totalTip = billAmount * (tipPercentage / 100);
+  //tip per person
+  const tipPerPerson = totalTip / numberofpeople;
+  //total per person
+  const totalPerPerson = (billAmount + totalTip) / numberofpeople;
+
+  // 4. Round to nearest
+  const tipDisplay = tipPerPerson.toFixed(2);
+  const totalDisplay = totalPerPerson.toFixed(2);
+  const totalTipFormatted = totalTip.toFixed(2);
+  const grandTotalFormatted = (billAmount + totalTip).toFixed(2);
+
+  // 5. Update UI
+  billTipAmount.innerHTML = `₹${tipDisplay}`;
+  billTotalPerPerson.innerHTML = `₹${totalDisplay}`;
+  totalTipDisplay.innerHTML = `₹${totalTipFormatted}`;
+  grandTotalDisplay.innerHTML = `₹${grandTotalFormatted}`;
+}
+
+// reset
+resetButton.addEventListener("click", resetEverything);
+function resetEverything() {
+  buttons.forEach((btn) => btn.classList.remove("selected"));
+  billTipAmount.innerHTML = "₹0.00";
+  billTotalPerPerson.innerHTML = "₹0.00";
+  totalTipDisplay.innerHTML = "₹0.00";
+  grandTotalDisplay.innerHTML = "₹0.00";
+  billAmount.value = "";
+  numberofpeople.value = "";
+  customTipPercentage.value = "";
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.activeElement.blur();
+  }
+});
